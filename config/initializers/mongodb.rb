b8eb4aa07a356861c10e98d5258058ad6e6b6018
Clark <giorgos@white-mac.org>
2011-11-29 12:44:25 +0200

    New mongodb init file.

--- config/initializers/mongodb.rb
+++ config/initializers/mongodb.rb
@@ -6,7 +6,7 @@ when "development"
 when "test"
   
 when "production"
-  MongoMapper.connection = Mongo::Connection.new('staff.mongohq.com/app1810029', 10034, { :logger => Rails.logger })
+  MongoMapper.connection = Mongo::Connection.new('staff.mongohq.com:10034/app1810029', nil, { :logger => Rails.logger })
   MongoMapper.database = 'jeocrowd4s_production'
   MongoMapper.database.authenticate('heroku', '448e011347c9ae6e757c88cfcc7ce670')
 end
