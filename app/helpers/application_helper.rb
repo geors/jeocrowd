a1686d59909a2c44ab02159689624a3c131dbe41
Clark <giorgos.lamprianidis@gmail.com>
2012-01-04 02:37:04 +0200

    load the geometry library from google maps api
    Signed-off-by: Clark <giorgos.lamprianidis@gmail.com>

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,11 @@
 module ApplicationHelper
   
   def include_google_maps
-    javascript_include_tag("http://maps.google.com/maps/api/js?sensor=#{sensor_enabled?}") if (map_needed?)
+    javascript_include_tag("http://maps.google.com/maps/api/js?#{gm_libraries}&sensor=#{sensor_enabled?}") if (map_needed?)
+  end
+
+  def gm_libraries
+    "libraries=geometry"
   end
 
   def map_needed?
