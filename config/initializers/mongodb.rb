2f957d3bf4c4a64cb7ce510b69192ec8c98ec069
glampr <lamprianidis@okeanos.gr>
2012-02-13 17:17:01 +0200

    okeanos+heroku config

--- config/initializers/mongodb.rb
+++ config/initializers/mongodb.rb
@@ -1,15 +1,28 @@
-case Rails.env
-when "development", "production"
-  MongoMapper.connection = Mongo::Connection.new('localhost', 27017, { :logger => Rails.logger })
+#case Rails.env
+#when "development"
+#  MongoMapper.connection = Mongo::Connection.new('localhost', 27017, { :logger => Rails.logger })
+#  MongoMapper.database = 'jeocrowd4s_development'
+#  # MongoMapper.database.authenticate('admin', 'sekret')
+#when "test"
+#  
+#when "production"
+#  MongoMapper.connection = Mongo::Connection.new('staff.mongohq.com', '10034', { :logger => #Rails.logger })
+#  MongoMapper.database = 'app1810029'
+#  MongoMapper.database.authenticate('heroku', '448e011347c9ae6e757c88cfcc7ce670')
+#end
+
+begin
+  MongoMapper.connection = Mongo::Connection.new('localhost', 27017,
+    { :logger => Rails.logger })
   MongoMapper.database = 'jeocrowd4s_development'
   # MongoMapper.database.authenticate('admin', 'sekret')
-when "test"
-  
-when "production2"
-  MongoMapper.connection = Mongo::Connection.new('staff.mongohq.com', '10034', { :logger => Rails.logger })
+rescue
+  MongoMapper.connection = Mongo::Connection.new('staff.mongohq.com', '10034',
+    { :logger => Rails.logger })
   MongoMapper.database = 'app1810029'
   MongoMapper.database.authenticate('heroku', '448e011347c9ae6e757c88cfcc7ce670')
 end
+
 
 class Hash
   
