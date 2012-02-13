ef9362c2f5c72e19923deeb371e6a78dc03b702a
glampr <lamprianidis@okeanos.gr>
2012-02-13 17:11:21 +0200

    okeanos production settings

--- Gemfile
+++ Gemfile
@@ -35,3 +35,8 @@ group :test do
   # Pretty printed test output
   gem 'turn', :require => false
 end
+
+group :production do
+  gem 'execjs'
+  gem 'therubyracer'
+end
--- config/initializers/mongodb.rb
+++ config/initializers/mongodb.rb
@@ -1,11 +1,11 @@
 case Rails.env
-when "development"
+when "development", "production"
   MongoMapper.connection = Mongo::Connection.new('localhost', 27017, { :logger => Rails.logger })
   MongoMapper.database = 'jeocrowd4s_development'
   # MongoMapper.database.authenticate('admin', 'sekret')
 when "test"
   
-when "production"
+when "production2"
   MongoMapper.connection = Mongo::Connection.new('staff.mongohq.com', '10034', { :logger => Rails.logger })
   MongoMapper.database = 'app1810029'
   MongoMapper.database.authenticate('heroku', '448e011347c9ae6e757c88cfcc7ce670')
